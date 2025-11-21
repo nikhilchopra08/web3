6ac66dec83a3ffcc0137124b2db73146f1df4c7d
Nikhil chopra <nikhil0080012005@gmail.com>
2025-11-22 00:57:34 +0530

    basic ui, right now the public keys of previous mnenomics are also visible will update it in next step

--- hd-webbased-wallet/src/App.jsx
+++ hd-webbased-wallet/src/App.jsx
@@ -1,12 +1,35 @@
 import { useState } from 'react'
 import './App.css'
+import { generateMnemonic, mnemonicToSeed } from "bip39";
+import { derivePath } from "ed25519-hd-key";
+import { SolanaWallet } from './SolanaWallet';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [mnemonic, setmnemonic] = useState("");
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [publicKeys, setPublicKeys] = useState([]);
 
   return (
     <>
-     
+     <button
+      onClick={async function(){
+        const mm = generateMnemonic();
+        setmnemonic(mm);
+        console.log(mnemonic);
+      }}
+      >
+        Create Seed Phrase
+     </button>
+
+      {mnemonic && (
+        <div>
+          <h3> Seed phrases </h3>
+          <p>{mnemonic}</p>
+        </div>
+      )}
+
+      <SolanaWallet mnemonic={mnemonic}/>
     </>
   )
 }
