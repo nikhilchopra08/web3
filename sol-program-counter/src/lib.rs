85c331db02ed71d511871a6c9bfb22adf0489ad9
Nikhil chopra <nikhil0080012005@gmail.com>
2025-12-10 10:48:03 +0530

    simple rust contract

---
+++ sol-program-counter/src/lib.rs
@@ -0,0 +1,49 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+
+use solana_program::{
+    account_info::{AccountInfo, next_account_info},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    pubkey::Pubkey,
+};
+
+entrypoint!(counter_contract);
+
+#[derive(BorshSerialize, BorshDeserialize)]
+enum InstructionType {
+    Increment(u32),
+    Decrement(u32),
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+struct Counter {
+    count: u32,
+}
+
+pub fn counter_contract(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let acc = next_account_info(&mut accounts.iter())?;
+
+    let instruction_type = InstructionType::try_from_slice(instruction_data)?;
+
+    let mut counter_data = Counter::try_from_slice(&acc.data.borrow())?;
+
+    match instruction_type {
+        InstructionType::Increment(value) => {
+            counter_data.count += value;
+        },
+        InstructionType::Decrement(value) => {
+            counter_data.count -= value;
+        }
+    }
+
+    counter_data.serialize(&mut *acc.data.borrow_mut());
+
+    msg!("contract finished");
+
+    Ok(())
+}
